a0679f211d6f3e03b3523c41b76e43d3bdea6937
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2013-07-31 21:54:27 +0100

    Finished workaround for #1 - added instructions
    The output .html file now contains text telling the user
    how to use the generated links

--- export_all_trello_boards.js
+++ export_all_trello_boards.js
@@ -1,20 +1,39 @@
 javascript: if (frames.length > 1) alert('Sorry, frames detected.');
 else {
+    // Maintenance Note:
+    //      Only use links to Trello download URLs, so user can download
+    //      everything on the page.
     var wnd = open('', 'lnkswnd', 'width=400,height=500,top=0,left=0,scrollbars,resizable');
     var lnks = document.links;
-    var hyperlinks_text = ''; // TODO Add some instructions, and hyperlinks to later sections
+    var preamble_text =
+        '<html><base target=_blank>\n';
+    var hyperlinks_text =
+        '<h2>Instructions for use</h2>' +
+        '<ol>' +
+        '   <li>Use a download manager to download all the links in this page' +
+        '   <li>As a temporary workaround for breaking Trello changes, do one of the following' +
+        '   <ul>' +
+        '      <li>Follow "Windows instructions" below' +
+        '      <li>Follow "Unix instructions" below' +
+        '   </ul>' +
+        '</ol>' +
+        '<hr>\n';
     var windows_rename_text =
-        '<hr><p>On Windows, put these commands into a .bat file, ' +
+        '<hr>' +
+        '<h2>Windows instructions</h2>' +
+        '<p>On Windows, put these commands into a .bat file, ' +
         'and run them, to rename the files saved from Trello, so that they ' +
         'are named after the board, instead of a crypic 8-character string</p>' +
         '<pre>';
     var unix_rename_text =
-        '<hr><p>On Unix, put these commands into a .bat file, ' +
+        '<hr>' +
+        '<h2>Unix instructions</h2>' +
+        '<p>On Unix, put these commands into a .bat file, ' +
         'and run them, to rename the files saved from Trello, so that they ' +
         'are named after the board, instead of a crypic 8-character string</p>' +
         '<pre>';
     with(wnd.document) {
-        var s = '<html><base target=_blank>\n';
+        var s = preamble_text;
         for (var i = 0; i < lnks.length; i++) {
             // urls look like: https://trello.com/b/nC8QJJoZ/trello-development
             var board_url = lnks[i].href;
