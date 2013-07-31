34f539e028cb3a6d8354be7308ec6e84cf97b133
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2013-07-31 21:37:38 +0100

    Add workaround for #1 - commands to rename files
    Append the .bat or .sh commands that the user can execute, to rename
    the saved files, so that they are named after their board_id, not the
    8-character
    "slug"

--- export_all_trello_boards.js
+++ export_all_trello_boards.js
@@ -2,6 +2,17 @@ javascript: if (frames.length > 1) alert('Sorry, frames detected.');
 else {
     var wnd = open('', 'lnkswnd', 'width=400,height=500,top=0,left=0,scrollbars,resizable');
     var lnks = document.links;
+    var hyperlinks_text = ''; // TODO Add some instructions, and hyperlinks to later sections
+    var windows_rename_text =
+        '<hr><p>On Windows, put these commands into a .bat file, ' +
+        'and run them, to rename the files saved from Trello, so that they ' +
+        'are named after the board, instead of a crypic 8-character string</p>' +
+        '<pre>';
+    var unix_rename_text =
+        '<hr><p>On Unix, put these commands into a .bat file, ' +
+        'and run them, to rename the files saved from Trello, so that they ' +
+        'are named after the board, instead of a crypic 8-character string</p>' +
+        '<pre>';
     with(wnd.document) {
         var s = '<html><base target=_blank>\n';
         for (var i = 0; i < lnks.length; i++) {
@@ -10,12 +21,18 @@ else {
             if (board_url.indexOf("https://trello.com/b/") != -1) {
                 var components = board_url.split("/");
                 var board_slug = components[4];
-                //var board_id = components[5];
+                var board_id = components[5];
 
                 var jason_url = "https://trello.com/1/boards/" + board_slug + "?fields=all&actions=all&action_fields=all&actions_limit=1000&cards=all&card_fields=all&card_attachments=true&lists=all&list_fields=all&members=all&member_fields=all&checklists=all&checklist_fields=all&organization=false";
-                s += '<li><a href=' + jason_url + '><span onClick=window.close()>' + lnks[i].text + '</span></a></li>\n';
+                hyperlinks_text += '<li><a href=' + jason_url + '><span onClick=window.close()>' + lnks[i].text + '</span></a></li>\n';
+                
+                windows_rename_text += "rename " + board_slug + " " + board_id + ".json\n";
+                unix_rename_text += "mv " + board_slug + " " + board_id + ".json\n";
             }
         }
+        s += hyperlinks_text;
+        s += windows_rename_text + '</pre>';
+        s += unix_rename_text + '</pre>';
         s += '</html>';
         writeln(s);
         void(close());
