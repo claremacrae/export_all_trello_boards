6809ce59f8e942a73d4b5be446cf2fc70075c6fe
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2013-07-31 08:10:48 +0100

    Refactored code, for clarity
    This separates out the unpacking of the trello board url and the
    creation of the url for downloading JSON

--- export_all_trello_boards.js
+++ export_all_trello_boards.js
@@ -5,14 +5,23 @@ else {
     with(wnd.document) {
         var s = '<html><base target=_blank>\n';
         for (var i = 0; i < lnks.length; i++) {
+            // urls look like: https://trello.com/board/welcome-board/4e6a8095efa69909ba007382/somename
             var board_url = lnks[i].href;
             if (board_url.indexOf("https://trello.com/board/") != -1) {
                 var components = board_url.split("/");
-                var jason_url = components[0] + "//";
-                jason_url += components[2] + "/";
-                jason_url += components[3] + "/";
-                jason_url += components[5] + "/";
-                jason_url += components[4];
+
+                var http_type = components[0];
+                // components[1] is empty
+                var trello_domain = components[2];
+                var board_keyword = components[3];
+                var board_slug = components[4];
+                var board_id = components[5];
+
+                var jason_url = http_type + "//";
+                jason_url += trello_domain + "/";
+                jason_url += board_keyword + "/";
+                jason_url += board_id + "/";
+                jason_url += board_slug;
                 jason_url += ".json";
                 s += '<li><a href=' + jason_url + '><span onClick=window.close()>' + lnks[i].text + '</span></a></li>\n';
             }
